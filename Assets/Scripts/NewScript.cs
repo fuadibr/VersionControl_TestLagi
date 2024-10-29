81371fdb34676c7283093aac940dfad0772b297a
fuadibr <ahmadfuad.ibrahim@gmail.com>
2024-10-29 10:00:07 +0800

    add NewSript

---
+++ Assets/Scripts/NewScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewScript : MonoBehaviour
+{
+    int test = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        test = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
