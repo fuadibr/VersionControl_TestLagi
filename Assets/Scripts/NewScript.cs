7a29a1e780255667249106bcae9036179c536458
fuadibr <ahmadfuad.ibrahim@gmail.com>
2024-10-29 10:01:50 +0800

    change test value from 1 to 2

--- Assets/Scripts/NewScript.cs
+++ Assets/Scripts/NewScript.cs
@@ -8,7 +8,7 @@ public class NewScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        test = 1;
+        test = 2;
     }
 
     // Update is called once per frame
